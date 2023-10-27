c589ce039abcce7e35d0f15ebb26f3d3dbdf6a7d
austinkelsay <austinkelsay@yahoo.com>
2023-10-27 15:40:10 -0500

    In initial

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const { bech32 } = require('bech32');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const cors = require('cors');
+const { connect, createInvoice } = require('./lnd');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.options('*', cors());
+
+connect();
+
+// Function to encode URL into bech32 format
+function encodeLnurl(url) {
+    const words = bech32.toWords(Buffer.from(url, 'utf8'));
+    return bech32.encode('lnurl', words, 2000).toUpperCase();
+}
+
+// Endpoint to retrieve the bech32 encoded LNURL and its QR code
+app.get('/getlnurl', async (req, res) => {
+    const originalUrl = `${process.env.BACKEND_URL}/lnurl`
+    const encodedLnurl = encodeLnurl(originalUrl);
+
+    res.json({
+        lnurl: encodedLnurl
+    });
+});
+
+app.get('/lnurl', (req, res) => {
+    const metadata = [
+        ["text/plain", "Sample LNURL-PAY endpoint"]
+    ];
+    const response = {
+        callback: `${process.env.BACKEND_URL}/callback`,
+        maxSendable: 1000000,  // milisatoshis
+        minSendable: 1000,     // milisatoshis
+        metadata: JSON.stringify(metadata),
+        tag: "payRequest"
+    };
+    res.json(response);
+});
+
+app.get('/callback', async (req, res) => {
+    const { amount } = req.query;
+
+    const metadata = [["text/plain", "Sample LNURL-PAY endpoint"]];
+    const hash = crypto.createHash('sha256').update(JSON.stringify(metadata), 'utf8').digest('hex');
+    
+    const memo = `LNURL-PAY ${hash}`;
+    // Convert amount from milisatoshis to satoshis
+    const value = parseInt(amount) / 1000;
+
+    const invoice = await createInvoice({ value, memo });
+
+    console.log(invoice);
+
+    const response = {
+        pr: invoice.payment_request,
+        routes: []
+    };
+
+    res.json(response);
+});
+
+app.post('/decode', (req, res) => {
+    const { lnurl } = req.body;
+    const { words } = bech32.decode(lnurl, 2000);
+    const requestByteArray = bech32.fromWords(words);
+    const decodedUrl = Buffer.from(requestByteArray).toString();
+    res.json({ decodedUrl });
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
---
+++ lnd.js
@@ -0,0 +1,61 @@
+const LndGrpc = require("lnd-grpc");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const options = {
+    lndconnectUri: process.env.LND_CONNECT_URI
+};
+
+const lnd = new LndGrpc(options);
+
+const connect = async () => {
+  try {
+    await lnd.connect();
+
+    if (lnd.state !== "active") {
+      throw new Error(
+        "LND did not reach 'active' state within the expected time"
+      );
+    }
+    
+    console.log(`LND gRPC connection state: ${lnd.state}`);
+
+    // Start the invoice event stream on successful connection
+    // We want to always be listening for invoice events while the server is running
+    invoiceEventStream();
+  } catch (e) {
+    console.log("error", e);
+  }
+};
+
+const createInvoice = async ({ value, memo }) => {
+  // Use the 'addInvoice' method from the Lightning service of the 'grpc' module to create an invoice.
+  // This method requires an object parameter with 'value' and 'memo' properties.
+  // This method is asynchronous, so we use 'await' to pause execution until it completes.
+  const invoice = await lnd.services.Lightning.addInvoice({
+    value: value,
+    memo: memo,
+  });
+
+  return invoice;
+};
+
+const invoiceEventStream = async () => {
+  await lnd.services.Lightning.subscribeInvoices({
+    add_index: 0,
+    settle_index: 0,
+  })
+    .on("data", async (data) => {
+      console.log("on data:", data);
+    })
+    .on("error", (err) => {
+      console.log(err);
+    });
+};
+
+module.exports = {
+  connect,
+  createInvoice,
+  invoiceEventStream,
+};
